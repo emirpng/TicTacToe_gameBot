dd46268a5a2982757dbf9aca2b95ef8e946197c9
Emir <marveldarkbull@gmail.com>
2022-12-27 15:25:23 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
-DEV_USER_ID = int(os.environ.get('DEV_USER_ID'))
+BOT_TOKEN = os.environ.get('5841942016:AAEUeDRIkS7IdlKKnNWHBme6T1GfKHQGeSM')
+DEV_USER_ID = int(os.environ.get('5784013817'))
